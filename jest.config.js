1c64d61bdc61e3af9b322262a7f877ee2809fccf
Rohit Dobariya <rohit.d@oolio.com>
2023-11-23 17:27:09 +0530

    fix: declaration

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   displayName: 'rn-use-modal',
   collectCoverage: true,
   collectCoverageFrom: ['**/*.{ts,tsx}'],
-  modulePathIgnorePatterns: ['lib'],
+  modulePathIgnorePatterns: ['dist'],
   coveragePathIgnorePatterns: ['node_modules', 'src/index.ts'],
   transform: {
     '^.+\\.js$': require.resolve('react-native/jest/preprocessor.js'),
